09ffab3094572b928639c8bfcb57369805e550d8
Elizabeth Fun <elizabethfun@gmail.com>
2018-03-22 22:22:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {[a]: "z", [b]:"y", [c]:"x"}; 
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return playlist.assign({} playlist, {[artistName]: songTitle})
+  return playlist.assign({} playlist, { [artistName]: songTitle})
   return playlist
 }
 
