6093e6d6bbdc004543b22647c56769682e71f35c
Elizabeth Fun <elizabethfun@gmail.com>
2018-03-22 22:01:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,10 @@
 var playlist = {a: "z", b:"y", c:"x"}; 
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({},{artistName, songTitle})
+playlist.assign({artistName, songTitle})
+return updatePlaylist
+  
 }
 
 function removeFromPlaylist(playlist, artistName){
-delete playlist.c;
-if(playlist===true)
-return playlist
 }
